7c1cadb779d9d321a10ca4ffeeeba71804844d5e
Andreas Rozek <A.Rozek@gmx.de>
2024-06-12 06:03:02 +0200

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {  	
+  	rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
+  },
+
+  worker: {
+    format: "es",
+  },
+})
