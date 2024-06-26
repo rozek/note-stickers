b5f997e2b8ca1122dfac58e02e6529fdd0d5f63b
Andreas Rozek <A.Rozek@gmx.de>
2024-06-26 11:42:39 +0200

    new build

--- dist/NoteStickers.js
+++ dist/NoteStickers.js
@@ -4,9 +4,6 @@
 *                                                                              *
 *******************************************************************************/
 import { quoted, ValuesAreEqual, ValuesDiffer, ValueIsArray, ValueIsListSatisfying, expectListSatisfying, } from 'javascript-interface-library';
-//import 'localforage-types'
-import * as _localforage from 'localforage';
-const localforage = _localforage; // *C* very weird hack
 import { html } from 'htm/preact';
 import { throwError, throwReadOnlyError, ValueIsBoard, ValueIsSticker, ValueIsName, ValueIsIdentifier, ValueIsGeometry, ValueIsError, SNS_Project, newId, removeIdsFrom, TemplateOfBehavior, groupedBehaviorEntryList, } from 'shareable-note-stickers';
 import { SNS_BoardView } from 'sns-boardview';
