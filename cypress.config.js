e29adf50a916dbd602a7628c3dd0f07f9be31d0c
Andreas Rozek <A.Rozek@gmx.de>
2024-07-10 18:02:43 +0200

    Cypress configuration

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  typescript: {
+    tsconfig: 'cypress.tsconfig.json'
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.{js,jsx,ts,tsx}',
+  },
+})
